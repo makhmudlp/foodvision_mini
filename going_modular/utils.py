0d928f670500f1e90e658e14ab7f337016fe9135
Makhmud Kholmuminov <123930780+makhmudlp@users.noreply.github.com>
2026-02-08 11:32:17 +0400

    Add files via upload

---
+++ going_modular/utils.py
@@ -0,0 +1,61 @@
+
+from typing import Dict, List, Tuple
+import matplotlib.pyplot as plt
+from pathlib import Path
+import torch
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+  """Saves a PyTorch model to a target directory.
+
+  Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+
+  Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+  """
+  # Create target directory
+  target_dir_path = Path(target_dir)
+  target_dir_path.mkdir(parents=True,
+                        exist_ok=True)
+
+  # Create model save path
+  assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+  model_save_path = target_dir_path / model_name
+
+  # Save the model state_dict()
+  print(f"[INFO] Saving model to: {model_save_path}")
+  torch.save(obj=model.state_dict(),
+             f=model_save_path)
+def plot_loss_curves(results: Dict[str, List[float]]):
+    loss=results["train_loss"]
+    test_loss=results["test_loss"]
+
+    accuracy=results["train_acc"]
+    test_accuracy=results["test_acc"]
+
+    epochs=range(len(results["test_acc"]))
+
+    plt.figure(figsize=(15,7))
+
+    plt.subplot(1,2,1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+
+    plt.subplot(1,2,2)
+    plt.plot(epochs, accuracy, label="train_acc")
+    plt.plot(epochs, test_accuracy, label="test_acc")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Acuuracy")
+    plt.legend()
